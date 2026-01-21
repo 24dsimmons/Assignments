869a44f48e83926663cdc2b358fe895488b3c548
Domo, Destroyer of Code <143213927+24dsimmons@users.noreply.github.com>
2026-01-21 12:44:38 -0500

    Create BookType Class Library and started to create the book class

---
+++ Assignment_2_Interacting_Classes/BookType/BookType/Book.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BookType
+{
+    public class Book
+    {
+        //Atributes for Books
+        public int BookID { get; set; }
+        public string BookTitle { get; set; }
+        public bool IsOut { get; set; }
+        public DateTime DateCheckedOut { get; set; }
+
+        //Enumerator's for Book Types
+        public enum BookType
+        {
+            Fiction,
+            NonFiction
+        }
+        public BookType TypeofBook { get; set; }
+        private List<string> TypeofBookhistory;
+
+        // Methods = An action that Classes can do
+
+        public Book()
+        {
+            BookID = 0;
+            BookTitle = "Unknown";
+            DateCheckedOut = DateTime.Now;
+        }
+
+    }
+
+}
