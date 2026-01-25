9f1fb54a6113b72ed236c072b995b5729eafd3ae
Domo, Destroyer of Code <143213927+24dsimmons@users.noreply.github.com>
2026-01-25 02:27:36 -0500

    small changes

--- Assignment_2_Interacting_Classes/BookType/BookType/Book.cs
+++ Assignment_2_Interacting_Classes/BookType/BookType/Book.cs
@@ -10,6 +10,8 @@ namespace BookType
         public int BookID { get; set; }
         public string BookTitle { get; set; }
         public bool IsOut { get; set; }
+        public string BookAuthor { get; set; }
+
         public DateTime DateCheckedOut { get; set; }
 
         //Enumerator's for Book Types
@@ -17,17 +19,46 @@ namespace BookType
         {
             Fiction,
             NonFiction
-        }
         public BookType TypeofBook { get; set; }
+
+        // Collections
         private List<string> TypeofBookhistory;
 
         // Methods = An action that Classes can do
 
         public Book()
+        // Constructor 
         {
             BookID = 0;
             BookTitle = "Unknown";
+            IsOut = false;
+            BookAuthor = "Unknown";
+            BookType = BookType.Fiction;
+            TypeofBookhistory = new List<string>();
+
+        }
+
+        public Book(int  bookID, string bookTitle , bool isOut, string BookAuthor, BookType bookType)
+        {
+            BookID = bookID;
+            BookTitle = bookTitle;
+            IsOut = isOut;
             DateCheckedOut = DateTime.Now;
+            TypeofBookhistory = new List<string>();
+            bookType.Add ($"Account opened on {DateCheckedOut}")
+        }
+
+        public virtual void Checkout (Boolean)
+        {
+            if (IsOut)
+            {
+                TypeofBookhistory.Add("Book is currently checkedout! Please Try again another time...")
+                return true; // When isout == true that means that the book is checked out! 
+            }
+
+            IsOut = false;
+            return TypeofBookhistory.Add("Book is now checkedout to you!")
+
         }
 
     }
