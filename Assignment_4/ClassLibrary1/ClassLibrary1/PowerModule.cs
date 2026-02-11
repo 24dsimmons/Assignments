70cfeb7ead582bf0188fc73a12cf86f44cc408ac
Domo, Destroyer of Code <143213927+24dsimmons@users.noreply.github.com>
2026-02-10 21:29:26 -0500

    Chore: Intial File Structure

---
+++ Assignment_4/ClassLibrary1/ClassLibrary1/PowerModule.cs
@@ -0,0 +1,23 @@
+﻿namespace MiniSmartHome
+
+{
+    public class PowerModule
+    {
+        public bool IsOnline { get; private set; }
+        public bool IsPoweredOn { get; private set; }
+
+        public void SetOnline(bool online)
+        {
+
+        }
+        public void TurnOn()
+        {
+
+        }
+
+        public void TurnOff()
+        {
+
+        }
+    }
+}
---
+++ Assignment_4/ClassLibrary1/ClassLibrary1/SmartLight.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniSmartHome
+{
+    internal class SmartLight
+    {
+    }
+}
