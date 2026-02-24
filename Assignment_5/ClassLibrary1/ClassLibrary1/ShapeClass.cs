6c585bfb783da3d8db07465520488096b09d4af9
Domo, Destroyer of Code <143213927+24dsimmons@users.noreply.github.com>
2026-02-24 17:31:55 -0500

    Assignment 5 initial commit

---
+++ Assignment_5/ClassLibrary1/ClassLibrary1/ShapeClass.cs
@@ -0,0 +1,85 @@
+﻿namespace ShapeSystem;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection.Metadata.Ecma335;
+using static ShapeSystem.ShapeClass;
+
+public class ShapeClass
+
+    {
+       
+    public static List<string> History = new List<string>();
+
+// This says that any class must agree to provide these methods! Like a contract between two parties, lists what methods a class must implement
+// Does not tell a class how those methods are implemented. 
+public interface ProcessShape
+    {
+        // This means that any shape we create must do two things:
+        // 1. Calculate the area
+        // 2. Return the area as a double
+        double GetArea();
+    }
+    public class Circle:ProcessShape
+    {
+        private double radius;
+         
+        public double GetArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
+    public class Rectangle : ProcessShape
+    {
+        private double width;
+        private double height;
+
+        public Rectangle(double w, double h) // Rectangle Constructor! reuns when a new rectangle is made. 
+        {
+            width = w;
+            height = h; //Defining Alliases for the corresponding values!
+        }
+
+        public double GetArea()
+        {
+            return width * height;
+        }
+
+        public class Triangle : ProcessShape
+        {
+            private double Base;
+            private double Height;
+
+            public Triangle(double b, double h)
+            {
+                Base = b;
+                Height = h;
+            }
+
+            public double GetArea()
+            {
+                return Base * Height / 2;
+            }
+        }
+        public interface Log
+        {
+             string GetReport();
+        }
+
+        public class Report : Log
+        {
+            public string GetReport() {
+                return $" DEBUG: Shape: {type} Area: {Area}, Date: {DateTime.Now}";
+        }
+     }
+    }
+
+
+
+        
+
+
+
+
