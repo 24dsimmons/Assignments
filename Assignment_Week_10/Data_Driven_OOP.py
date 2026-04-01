ff19b677dde3ec5bb2e6a3cc3dadf59def129eac
Domo, Destroyer of Code <143213927+24dsimmons@users.noreply.github.com>
2026-04-01 12:08:01 -0400

    Setting Up PT1

---
+++ Assignment_Week_10/Data_Driven_OOP.py
@@ -0,0 +1,7 @@
+# GOALS:
+# Come up with a thesis/theroy and build a small oop application that.
+#1. Pulls real data from a free API or open dataset
+#2. store/transforms it using an appropriate data structures.
+#3. Outputs evidence for or against your thesis. 
+
+
