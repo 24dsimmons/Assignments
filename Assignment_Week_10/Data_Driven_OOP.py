9d35d801d103ac0e32dabd19c8582a303e73b261
Domo, Destroyer of Code <143213927+24dsimmons@users.noreply.github.com>
2026-04-06 11:46:35 -0400

    Testing API usage

--- Assignment_Week_10/Data_Driven_OOP.py
+++ Assignment_Week_10/Data_Driven_OOP.py
@@ -5,3 +5,10 @@
 #3. Outputs evidence for or against your thesis. 
 
 
+import json
+import urllib.request
+
+url = "http://api.open-notify.org/astros.json"
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
+print(result)
